56bf82f1a77300e854b61e291e7d1992eb626f02
ibrahimhouzairati <ibrahimhouzairati@gmail.com>
2025-12-05 14:05:21 +0100

    Menu

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <header>
+        <nav>
+            <a href="#accueil">Accueil</a>
+            <a href="Contactez-moi">Contactez-moi</a>
+        </nav>
+</header>
+</head>
+<body>
+    
+</body>
+</html>
